8162fc60aa509134b73405e40346f874e97c0d6e
somite <30598033+somite@users.noreply.github.com>
2021-03-03 16:31:24 +0800

    貓咪演化

---
+++ cat/Assets/scripts/spawnManager.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class spawnManager : MonoBehaviour
+{
+    //欄位
+    //時間
+    //int整數
+    //float浮點數
+
+    //欄位
+    //類型  名稱  指定值;
+    //public顯示在面板
+    public float interval = 2.5f;
+    //存放物件可以用GameObject
+    public GameObject apple;
+
+    //事件
+    //開始事件
+    private void Start()
+    {
+        //輸出API
+        spawn();
+    }
+    //自訂函式
+    //語法
+    public void spawn()
+    {
+        Instantiate(apple);
+    }
+
+
+
+        }
