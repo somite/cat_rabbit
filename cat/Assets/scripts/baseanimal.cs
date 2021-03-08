416ad4991d24ed70156802990c7058809d32e960
張逸紳 <30598033+somite@users.noreply.github.com>
2021-03-08 16:53:30 +0800

    生成跟動物基底

---
+++ cat/Assets/scripts/baseanimal.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class baseanimal : MonoBehaviour
+{
+    [Header("行動速度")]
+    public float speed;
+    [Header("生命")]
+    public float lifetime;
+    [Header("吃幾隻")]
+    public int eatCount;
+    [Header("追蹤目標")]
+    public string targetName;
+    [Header("目標物件陣列")]
+    public GameObject[] targets;
+
+
+    /// <summary>
+    /// 搜尋目標
+    /// </summary>
+    private void FindTarget()
+    {
+       //遊戲物件陣列
+       targets= GameObject.FindGameObjectsWithTag(targetName);
+    }
+
+    private void Start()
+    {
+        FindTarget();
+    }
+}
--- cat/Assets/scripts/spawnManager.cs
+++ cat/Assets/scripts/spawnManager.cs
@@ -11,22 +11,39 @@ public class spawnManager : MonoBehaviour
     //欄位
     //類型  名稱  指定值;
     //public顯示在面板
+    [Header("間隔")]
     public float interval = 2.5f;
+    public GameObject obj;
     //存放物件可以用GameObject
-    public GameObject apple;
-
+    
+    public Vector2 posMin = new Vector2(-8, -4);
+    public Vector2 posMax = new Vector2(+8, +4);
+    //第一次生成的數量
+    public int count;
+    [Header("是否需要間格生成")]
+    public bool needSpawn;
+    [Header("目前物件的數量")]
+    public int currentCount;
     //事件
     //開始事件
     private void Start()
     {
-        //輸出API
-        spawn();
+        for (int i = 0; i < count; i++)
+        {
+            spawn();
+        }
+        //重複呼叫函式名稱, 開始時間, 間隔時間
+        if (needSpawn) InvokeRepeating("spawn", interval, interval);
     }
     //自訂函式
     //語法
     public void spawn()
     {
-        Instantiate(apple);
+        currentCount++;
+        float x = Random.Range(posMin.x, posMax.x);
+        float y = Random.Range(posMin.y, posMax.y);
+
+        Instantiate(obj,new Vector2(x,y), Quaternion.identity);
     }
 
 
